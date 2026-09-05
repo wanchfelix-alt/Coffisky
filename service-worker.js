8f9bb229b7e92a67c7d94658e48883fc5393d4ba
wanchfelix-alt <wanchfelix@gmail.com>
2026-09-05 22:54:30 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cellar-cup-v7';
+const CACHE_NAME = 'cellar-cup-v9';
 const ASSETS = ['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png'];
 const PASSTHROUGH_DOMAINS = ['openfoodfacts.org','ocr.space'];
 
